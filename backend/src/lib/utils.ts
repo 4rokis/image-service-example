c28afac9f88a3c06da4a95df6fd840fb548e4762
Bogdan Sikora <bogdan@arokis.me>
2023-01-16 18:32:15 +0100

    fix: unused code removed

--- backend/src/lib/utils.ts
+++ backend/src/lib/utils.ts
@@ -1,20 +1,6 @@
-export const rotated0 = (width: number, height: number): [number, number] => {
-  const x = width / 2
-  const y = height / 2
-
-  return rotatePointMinus90(x, y)
-}
-export const rotatePointMinus90 = (x: number, y: number): [number, number] => {
-  return [-y + x, y - x]
-}
 export const isDefined = (x: any) => Boolean(x) || x === 0
 export const isNumber = (x: any) =>
   isDefined(x) && (Boolean(Number(x)) || Number(x) === 0)
-
-export const isURLExists = async (url: string) => {
-  const res = await fetch(url, { method: 'HEAD' })
-  return res.ok
-}
 
 export const extractFileName = (path: string) => {
   const pathParts = path.split('/')
